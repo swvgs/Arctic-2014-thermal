597ba7ba1b512c077380e4dab443fde366ac9670
erebus9856 <dblake@swvgs.us>
2014-02-09 00:02:33 -0500

    added commends to the python files

--- code/python/readfiles.py
+++ code/python/readfiles.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
-import glob
-import csv
+import glob #import global vars for the computer so we can get the directory list
 
-file_length = 0
+file_length = 0 #var to count lines in the file
 
-filenames = glob.glob('*.TXT')
-print filenames
+filenames = glob.glob('*.TXT') #make an array that has all the .TXT files in the directly listed in it
+print filenames #print the file names
 
-filenames_length = len(filenames)
+filenames_length = len(filenames) #count the number of file names
 
-
+# This function will find the median. Used this for the time since the array is not sorted. Since the array is 30, it will always output a half number.
 def getMedian(numericValues):
 	theValues = sorted(numericValues)
 	if len(theValues) % 2 == 1:
@@ -20,44 +19,44 @@ def getMedian(numericValues):
 		return (float(lower + upper)) / 2
 
 
-for fnames in filenames:
-	print fnames
-	read_file = open(fnames)
-	for line in read_file:
-		file_length += 1
-		print file_length
+for fnames in filenames: #loop through all the files found above
+	print fnames #print the current file name
+	read_file = open(fnames) #open and read the data from the current file
+	for line in read_file: #loop through all the lines in the file
+		file_length += 1 #incriment our number of lines counter
+		print file_length #print the file length
 		#if file_length > 1:
 		#	print file_length,':',line
 #		print line
-		if 'Data collection stopped.' in line:
-			print 'End of data stream.'
-			break
-	       	a=line.split(';')
+		if 'Data collection stopped.' in line: #check to see if we stopped collecting data or not
+			print 'End of data stream.' #print a happy message letting us know we found the end of the file
+			break #exit the loop
+	       	a=line.split(';') #split the line at all the semicolons
 #		print a
-		if file_length > 0:
-			time = a[1].split(',')
-			time2 = [float(col) for col in time]
-			amb = a[2].split(',')
-			amb2 = [float(col) for col in amb]
-			tempir = a[3].rstrip('\r\n')
-			ir = tempir.split(',')
-			ir2 = [float(col) for col in ir]
-		mark = a[0]
-		print mark
-		if file_length >0:
-			avg_time = sum(time2)/float(len(time2))
-			avg_time = getMedian(time2)
-			avg_amb = sum(amb2)/float(len(amb2))
-			avg_ir = sum(ir2)/float(len(ir2))
-#			print time2
+		if file_length > 0: #this will let us disreguard the array header if needed
+			time = a[1].split(',') #split the time into an array
+			time2 = [float(col) for col in time] #convert time from strings to floats
+			amb = a[2].split(',') #split the ambient temperatures into an array
+			amb2 = [float(col) for col in amb] #convert the ambient temperatures from strings to floats
+			tempir = a[3].rstrip('\r\n') #get rid of the line end for the IR last item
+			ir = tempir.split(',') #break the IR temperatures into an array
+			ir2 = [float(col) for col in ir] #convert the IR temperatures from strings to floats
+		mark = a[0] #put the marks into its own little special variable
+		print mark #print the mark
+		if file_length >0:#check to skip the header again if needed (probably can combine this loop)
+			avg_time = sum(time2)/float(len(time2)) #average the time
+			avg_time = getMedian(time2) #find the median time (overwrites the above line)
+			avg_amb = sum(amb2)/float(len(amb2)) #average the ambient temperatures
+			avg_ir = sum(ir2)/float(len(ir2)) #average the IR temperatures
+#			print time2 #print everything out 
 			print avg_time
 #			print amb2
 			print avg_amb
 #			print ir2
 			print avg_ir
-			print '------------------'
-	print "\nFile length is %d lines." %file_length
-	file_length = 0
+			print '------------------' #print a line so we can see the breaks easier
+	print "\nFile length is %d lines." %file_length #tell us how many lines are in the file
+	file_length = 0 #reset the variable for the next file.
 
 
 
